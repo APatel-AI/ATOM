42d10f6a896975a7502a09e3b83abce5e538bd8a
Arpan Patel <arpan.p.se@gmail.com>
2023-01-17 13:42:45 -0600

    added atom components

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ const scene = new THREE.Scene();
 
 // create our sphere or the ball. 
 // below the 3 is the radius and the 64's represent the height and width segements. 
-const geometry = new THREE.SphereGeometry( 3, 64, 64 );
+const geometry = new THREE.SphereGeometry( 1, 64, 64 );
 
 
 // creating the 1st ring: 
-const ring1 = new THREE.TorusGeometry( 8, 1.2, 30, 100 );
+const ring1 = new THREE.TorusGeometry( 7.8, .5, 30, 100 );
 const ring1_material = new THREE.MeshStandardMaterial( {
   color: 0xFFFFFF, 
   roughness: 0.1, 
@@ -21,7 +21,7 @@ const torus = new THREE.Mesh( ring1, ring1_material );
 scene.add( torus );
 
 // creating the 2nd ring: 
-const ring2 = new THREE.TorusGeometry( 6, 1.0, 30, 100 );
+const ring2 = new THREE.TorusGeometry( 7.4, .5, 30, 100 );
 const ring2_material = new THREE.MeshStandardMaterial( {
   color: 0xFFFFFF, 
   roughness: 0.1, 
@@ -32,7 +32,7 @@ scene.add( disc2 );
 
 
 // creating the 3rd ring: 
-const ring3 = new THREE.TorusGeometry( 5, 0.8 , 30, 100 );
+const ring3 = new THREE.TorusGeometry( 7.0, 0.5 , 30, 100 );
 const ring3_material = new THREE.MeshStandardMaterial( {
   color: 0xFFFFFF, 
   roughness: 0.1, 
@@ -41,7 +41,7 @@ const disc3 = new THREE.Mesh( ring3, ring3_material );
 scene.add( disc3 );
 
 // creating the 4th ring: 
-const ring4 = new THREE.TorusGeometry( 4, 0.6, 30, 100 );
+const ring4 = new THREE.TorusGeometry( 6.6, 0.5, 30, 100 );
 const ring4_material = new THREE.MeshStandardMaterial( {
   color: 0xFFFFFF, 
   roughness: 0.1, 
@@ -51,14 +51,47 @@ const disc4 = new THREE.Mesh( ring4, ring4_material );
 scene.add( disc4 );
 
 
+// creating the 5th ring: 0.4 difference in size from the previous ring 
+const ring5 = new THREE.TorusGeometry( 6.2, 0.5, 30, 100 );
+const ring5_material = new THREE.MeshStandardMaterial( {
+  color: 0xFFFFFF, 
+  roughness: 0.1, 
+} );
+const disc5 = new THREE.Mesh( ring5, ring5_material );
+
+scene.add( disc5 );
+
+// creating the 6th ring: 0.4 difference in size from the previous ring 
+const ring6 = new THREE.TorusGeometry( 5.8, 0.5, 30, 100 );
+const ring6_material = new THREE.MeshStandardMaterial( {
+  color: 0xFFFFFF, 
+  roughness: 0.1, 
+} );
+const disc6 = new THREE.Mesh( ring6, ring6_material );
+
+scene.add( disc6 );
+
+// creating the 7th ring: 0.4 difference in size from the previous ring 
+const ring7 = new THREE.TorusGeometry( 5.4, 0.5, 30, 100 );
+const ring7_material = new THREE.MeshStandardMaterial( {
+  color: 0xFFFFFF, 
+  roughness: 0.1, 
+} );
+const disc7 = new THREE.Mesh( ring7, ring7_material );
+
+scene.add( disc7 );
+
+
 //add texture or looks to the ball
 const material = new THREE.MeshStandardMaterial( { 
-  color: "#00ff83",
+  color: "#ffb700",
   roughness: 0.5,
  } )
 
 const mesh = new THREE.Mesh(geometry, material); 
 scene.add(mesh)
+
+
 
 
 //sizes
@@ -132,6 +165,9 @@ tl.fromTo('title', {opacity:0}, {opacity:1})
 // ring animation 
 function animate() {
   requestAnimationFrame (animate);
+  torus.rotation.x += 0.02;
+	torus.rotation.y += 0.02;
+
   disc2.rotation.x += 0.03;
 	disc2.rotation.y += 0.03;
 
@@ -139,7 +175,16 @@ function animate() {
   disc3.rotation.y += 0.04; 
 
   disc4.rotation.x += 0.05;
-  disc4.rotation.y += 0.05; 
+  disc4.rotation.y += 0.05;
+  
+  disc5.rotation.x += 0.06;
+  disc5.rotation.y += 0.06;  
+
+  disc6.rotation.x += 0.07;
+  disc6.rotation.y += 0.07;
+
+  disc7.rotation.x += 0.08;
+  disc7.rotation.y += 0.08;  
   renderer.render(scene, camera);
 }
 
